fc5493c84c9017256d2e9a62b4ad3eb123372d29
vladdimirrovv <95929583+vladdimirrovv@users.noreply.github.com>
2025-05-30 10:11:28 +0300

    processes.c

--- src/processes.c
+++ src/processes.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <ctype.h>
-#include "processes.h"
 #include <unistd.h>
 
-void print_processes() {
-    DIR *dir;
-    struct dirent *entry;
-
-    dir = opendir("/proc");
-    if (!dir) {
-        perror("opendir");
-        return;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (isdigit(entry->d_name[0])) {
-            printf("PID: %s\n", entry->d_name);
-        }
-    }
-
-    closedir(dir);
+void processes(){
+    FILE* fp = popen("ps -eo pid,command --sort=pid", "r");
+    if (fp == NULL) fprintf(stderr, "Error of opening file");
+    char buf[100];
+    while(fgets(buf, 100, fp) != NULL) fprintf(stdout, "%s", buf);
+    pclose(fp);
 }
