a9426d1c8afbfc38819ab02ec90f758e9daf95df
vladdimirrovv <95929583+vladdimirrovv@users.noreply.github.com>
2025-05-30 10:10:46 +0300

    help.c

--- src/help.c
+++ src/help.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "help.h"
 #include <unistd.h>
 
-void print_help() {
-    printf("Usage: console_utility [options]\n");
-    printf("Options:\n");
-    printf("  -u, --users       Print list of users and their home directories\n");
-    printf("  -p, --processes   Print list of running processes\n");
-    printf("  -l PATH, --log PATH   Redirect output to the specified log file\n");
-    printf("  -e PATH, --errors PATH   Redirect errors to the specified file\n");
-    printf("  -h, --help        Print this help message\n");
+void help(){
+    printf("Usage [options]\n");
+    printf("-u, --users		show list of users and their home directories\n");
+    printf("-p, --processes	        show list of running processes\n");
+    printf("-l PATH, --log PATH	redirect output to the file\n");
+    printf("-e PATH, --errors PATH	redirect error output to the file\n");
+    printf("-h, --help		show this help message\n");
 }
