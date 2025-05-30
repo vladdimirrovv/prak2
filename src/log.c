294dc2d17d4c61ccb6e4d7b27baf5b97652f3880
vladdimirrovv <95929583+vladdimirrovv@users.noreply.github.com>
2025-05-30 10:11:10 +0300

    log.c

--- src/log.c
+++ src/log.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "log.h"
 #include <unistd.h>
-static FILE *log_file = NULL;
+#include <stdlib.h>
 
-void set_log_file(const char *path) {
-    if (log_file) {
-        fclose(log_file);
+void logging(const char* lfp){
+    FILE* fp;
+    fp = fopen(lfp, "w");
+    if ((fp = fopen(lfp, "w")) == NULL){
+	fprintf(stderr, "can't open log file for writing\n"); 
+	exit(1);
     }
-    log_file = fopen(path, "w");
-    if (!log_file) {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
-    dup2(fileno(log_file), 1);
+    if (dup2(fileno(fp), 1) == -1)
+        fprintf(stderr, "can't redirect output\n");
+    fclose(fp);
 }
