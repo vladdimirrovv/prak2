dc3aafec7d3b974e841830dedb535affe0293b10
vladdimirrovv <95929583+vladdimirrovv@users.noreply.github.com>
2025-05-30 10:12:49 +0300

    log.h

--- include/log.h
+++ include/log.h
@@ -1,7 +1,5 @@
-#ifndef LOG_H
-#define LOG_H
-
-void set_log_file(const char *path);
-void log_message(const char *message);
-
-#endif // LOG_H
+#ifndef Log_h
+#define Log_h
+#include <stdio.h>
+void logging(const char* lfp);
+#endif
